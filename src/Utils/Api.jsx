c0e1dc7d5748cf47e59ee44a9a323f12fbc7d90d
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-09-28 14:35:18 +0530

    changes

--- src/Utils/Api.jsx
+++ src/Utils/Api.jsx
@@ -6,4 +6,4 @@ export const UserApi = 'https://snapsage.site/'
 export const VendorApi = 'https://snapsage.site/vendor'
 export const AdminApi = 'https://snapsage.site/admin'
 
-// export const socketApi ='http://localhost:5000'
+export const socketApi ='https://snapsage.site/'
