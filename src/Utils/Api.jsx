6879a9ae2f3b0e87d3eb8b06ae46435213d879c8
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-09-28 14:33:43 +0530

    changes after deployed

--- src/Utils/Api.jsx
+++ src/Utils/Api.jsx
@@ -1,5 +1,9 @@
-export const UserApi = "http://localhost:5000"
-export const VendorApi = "http://localhost:5000/vendor"
-export const AdminApi = "http://localhost:5000/admin"
+// export const UserApi = "http://localhost:5000"
+// export const VendorApi = "http://localhost:5000/vendor"
+// export const AdminApi = "http://localhost:5000/admin"
 
-export const socketApi ='http://localhost:5000'
+export const UserApi = 'https://snapsage.site/'
+export const VendorApi = 'https://snapsage.site/vendor'
+export const AdminApi = 'https://snapsage.site/admin'
+
+// export const socketApi ='http://localhost:5000'
